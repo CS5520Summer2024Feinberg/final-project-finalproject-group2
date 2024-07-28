df6a1c7d0eeb7651f49c3b8057dc1f5de37c6c24
卢勇霏 <luyongfei@s-MacBook-Pro.local>
2024-07-28 15:46:24 -0700

    add dependencies for suggestion module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+//    implementation("com.github.igalata:Bubble-Picker:v0.2.4")
+    // Solving Duplicate Class Error
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation("com.github.hantrungkien:Bubble-Picker:v1.0.5.2")
 }
